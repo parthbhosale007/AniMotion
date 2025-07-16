18200591ad49ea7bc4a2ee1f90ef1abe4194f89d
parthbhosale007 <109072879+parthbhosale007@users.noreply.github.com>
2025-07-16 11:54:23 +0530

    Add files via upload

---
+++ motion-to-anim/scripts/ex.py
@@ -0,0 +1,16 @@
+dict = {
+    "name":"Khandu",
+    "Rollno.":6,
+    "Branch":"AI&DS"
+}
+for key , value in dict.items():
+ print(key , ":" , value)
+
+for key in dict.keys():
+ print(key)
+
+for key in dict.keys():
+ print(key)
+
+for value in dict.values():
+ print(value)
